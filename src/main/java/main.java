dd73a57834c56116bd74030a4b4f4ec93840e575
Iroha7 <141012836+Iroha7@users.noreply.github.com>
2023-07-31 16:31:48 +0800

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class main {
+    public static void main(String[] args) {
+       String str;
+       int n;
+       str = "Cool Boy is Boy";
+       char c = str.charAt(0);
+        System.out.println(c);
+    }
+}
